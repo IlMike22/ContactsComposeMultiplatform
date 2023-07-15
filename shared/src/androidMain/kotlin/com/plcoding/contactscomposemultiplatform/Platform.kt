75773cd352ccc0c3da6c137c35bf7460a984c749
IlMike22 <michaelwidlok@gmail.com>
2023-07-15 08:18:55 +0200

    start with contacts definition and image utils

--- shared/src/androidMain/kotlin/com/plcoding/contactscomposemultiplatform/Platform.kt
+++ shared/src/androidMain/kotlin/com/plcoding/contactscomposemultiplatform/Platform.kt
@@ -1,5 +1,7 @@
 package com.plcoding.contactscomposemultiplatform
 
+import com.google.firebase.database.core.Platform
+
 class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
